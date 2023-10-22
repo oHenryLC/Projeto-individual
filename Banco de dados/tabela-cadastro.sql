706c039a2a267725cedcb5178d11692f2965c559
oHenryLC <henrique.conde@sptech.school>
2023-10-22 20:01:42 -0300

    Atualizando BD, finalizando repetição, e comentarios no código

--- Banco de dados/tabela-cadastro.sql
+++ Banco de dados/tabela-cadastro.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE projetoindividual;
 
-USE projetoindividual;
+USE projetoindividual; 
 
 CREATE TABLE usuario(
 idUsuario int primary key auto_increment,
@@ -20,4 +20,43 @@ CREATE TABLE aviso (
 
 SELECT * FROM usuario;
 
-SELECT COUNT(*) AS Preferidos, personagem FROM usuario GROUP BY personagem
+SELECT COUNT(*) AS Preferidos, personagem FROM usuario GROUP BY personagem;
+
+CREATE VIEW personagens AS SELECT COUNT(*) AS Preferidos, personagem FROM usuario GROUP BY personagem;
+
+select * from personagens;
+
+CREATE TABLE patrocinador (
+idPatrocinador INT PRIMARY KEY auto_increment,
+nome VARCHAR(45),
+descrição VARCHAR(300)
+);
+
+CREATE TABLE numero(
+numero INT primary KEY 
+);
+
+CREATE TABLE copaPistao(
+idCopa INT primary key auto_increment,
+dtAno DATE,
+lugar VARCHAR(45)
+);
+
+CREATE TABLE carro(
+idCarro INT primary key auto_increment,
+nome varchar(45),
+idade int,
+modelo varchar(45),
+fkNumero INT,
+constraint fknum foreign key (fkNumero) references numero(numero),
+fkPatrocinador INT,
+constraint fkpatro foreign key (fkPatrocinador) references patrocinador(idPatrocinador)
+);
+
+CREATE TABLE vencedores (
+idVencedores INT primary KEY auto_increment,
+fkCopa int,
+constraint fkcopapistao foreign key(fkCopa) references copaPistao(idCopa),
+fkCarro int,
+constraint fkcarrovencedor foreign key(fkCarro) references carro(idCarro)
+);
--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -19,82 +19,44 @@ personagem VARCHAR(30)
 );
 
 CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(idUsuario)
+id INT PRIMARY KEY AUTO_INCREMENT,
+titulo VARCHAR(100),
+descricao VARCHAR(150),
+fk_usuario INT,
+FOREIGN KEY (fk_usuario) REFERENCES usuario(idUsuario)
 );
 
--- create table aquario (
--- /* em nossa regra de negócio, um aquario tem apenas um sensor */
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	descricao VARCHAR(300)
--- );
+CREATE TABLE patrocinador (
+idPatrocinador INT PRIMARY KEY auto_increment,
+nome VARCHAR(45),
+descrição VARCHAR(300)
+);
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+CREATE TABLE numero(
+numero INT primary KEY 
+);
 
--- create table medida (
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	dht11_umidade DECIMAL,
--- 	dht11_temperatura DECIMAL,
--- 	luminosidade DECIMAL,
--- 	lm35_temperatura DECIMAL,
--- 	chave TINYINT,
--- 	momento DATETIME,
--- 	fk_aquario INT,
--- 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
--- );
+CREATE TABLE copaPistao(
+idCopa INT primary key auto_increment,
+dtAno DATE,
+lugar VARCHAR(45)
+);
 
+CREATE TABLE carro(
+idCarro INT primary key auto_increment,
+nome varchar(45),
+idade int,
+modelo varchar(45),
+fkNumero INT,
+constraint fknum foreign key (fkNumero) references numero(numero),
+fkPatrocinador INT,
+constraint fkpatro foreign key (fkPatrocinador) references patrocinador(idPatrocinador)
+);
 
-/*
-comando para sql server - banco remoto - ambiente de produção
-*/
-
--- CREATE TABLE usuario (
--- 	id INT PRIMARY KEY IDENTITY(1,1),
--- 	nome VARCHAR(50),
--- 	email VARCHAR(50),
--- 	senha VARCHAR(50),
--- );
-
--- CREATE TABLE aviso (
--- 	id INT PRIMARY KEY IDENTITY(1,1),
--- 	titulo VARCHAR(100),
--- 	descricao VARCHAR(150),
--- 	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
--- );
-
--- create table aquario (
--- /* em nossa regra de negócio, um aquario tem apenas um sensor */
--- 	id INT PRIMARY KEY IDENTITY(1,1),
--- 	descricao VARCHAR(300)
--- );
-
--- /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
--- CREATE TABLE medida (
--- 	id INT PRIMARY KEY IDENTITY(1,1),
--- 	dht11_umidade DECIMAL,
--- 	dht11_temperatura DECIMAL,
--- 	luminosidade DECIMAL,
--- 	lm35_temperatura DECIMAL,
--- 	chave TINYINT,
--- 	momento DATETIME,
--- 	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
--- );
-
--- /*
--- comandos para criar usuário em banco de dados azure, sqlserver,
--- com permissão de insert + update + delete + select
--- */
-
--- CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
--- WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
--- DEFAULT_SCHEMA = dbo;
-
--- EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
--- @membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
-
--- EXEC sys.sp_addrolemember @rolename = N'db_datareader',
--- @membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+CREATE TABLE vencedores (
+idVencedores INT primary KEY auto_increment,
+fkCopa int,
+constraint fkcopapistao foreign key(fkCopa) references copaPistao(idCopa),
+fkCarro int,
+constraint fkcarrovencedor foreign key(fkCarro) references carro(idCarro)
+);
